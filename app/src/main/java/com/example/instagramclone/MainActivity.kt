97802cdf6372b49e8cd2be93a5ba0ace82d5aa97
Anvar <anvarsv963@gmail.com>
2020-01-20 22:10:26 +0300

    add layout login

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.instagramclone
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : BaseActivity(0) {
 
     private val TAG = "MainActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_likes)
         setupBottomNavigation()
         Log.d(TAG, "oncreate")
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+//        auth.signInWithEmailAndPassword("arovanvar1@gmail.com", "Password1234")
+//            .addOnCompleteListener{
+//                if (it.isSuccessful) {
+//                    Log.d(TAG, "singIn: success")
+//                } else {
+//                    Log.e(TAG, "sing: failer", it.exception)
+//                }
+//            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
